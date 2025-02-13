2929ebec3e36046f5672c983d333c9562b929477
dkishorkoirala <reyzual.info@gmail.com>
2025-02-13 13:37:57 +0545

    Day 2 previous was day 1 not 2 by the way

--- python02/09exercise_5_2.py
+++ python02/09exercise_5_2.py
@@ -4,13 +4,13 @@ smallest = None
 while True:
     num = input("Enter a number: ")
     
-    if num.lower() == "done":  # Exit condition
+    if num.lower() == "done":
         break
     
     try:
-        num = int(num)  # Convert input to integer
+        num = int(num)
     except ValueError:
-        print("Invalid input")  # Handle non-integer input
+        print("Invalid input") 
         continue
     
     if largest is None or num > largest:
